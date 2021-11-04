79ff08bf189b9aaa1980f806220429d961729479
ridwansulaiman <87976706+ridwansulaiman@users.noreply.github.com>
2021-11-04 10:43:59 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "poc-danamon-devsecops"
+  region  = "asia-southeast2"
+}
+
+resource "google_compute_firewall" "tunnel" {
+  name    = "allow-tunnel"
+  network = "default"
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_instance" "tunnel" {
+  name          = "tunnel-instance"
+  machine_type  = "f1-micro"
+  zone          = "asia-southeast2-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
