e844961bb65d8fb6b3c1c7296cb35985c41c65b2
ridwansulaiman <87976706+ridwansulaiman@users.noreply.github.com>
2021-11-04 10:56:16 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ resource "google_compute_instance" "tunnel" {
     initialize_params {
       image = "ubuntu-1804-lts"
     }
-  network_interface {
+  }
+    network_interface {
     network = "default"
 
     access_config {
